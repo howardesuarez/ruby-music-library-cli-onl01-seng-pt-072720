5d984b7b85e3e299cbd076ecc3dbf6a376bd9a9a
howard suarez <howardesuarez@gmail.com>
2020-09-13 06:04:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -46,12 +46,10 @@ class MusicLibraryController
 
   def list_songs
     Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
-      puts 
-    
-  
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
     # Song.all.each_with_index {|song, i| puts "#{i+1}. #{song.artist} - #{song.name} - #{song.genre}"}
-    binding.pry
+    # binding.pry
   end
 
   def list_artists
@@ -107,3 +105,7 @@ class MusicLibraryController
 
 
 end
+
+# require_relative './config/environment'
+# controller = MusicLibraryController.new('./spec/fixtures/mp3s')
+# controller.play_song
